bb48b32022b3864434633b7d0ac5583e89637ce6
MariusTBjercke <marius@bjerckemedia.no>
2022-01-12 13:30:09 +0100

    Add project files.

---
+++ Moodle/GameCharacter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodle
+{
+    internal class GameCharacter
+    {
+
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public string Strength { get; set; }
+        public int Stamina { get; set; }
+        public int MaxStamina { get; set; }
+
+        public GameCharacter(string name, int health, string strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            MaxStamina = stamina;
+        }
+
+        public async Task Fight(GameCharacter player2)
+        {
+            var random = new Random();
+
+            if (Stamina == 0)
+            {
+                Recharge();
+                Console.WriteLine(Name + " har ikke nok stamina. Bruker 'recharge'.");
+            }
+            else
+            {
+                int newStrength;
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (Strength.Contains('-'))
+                {
+                    var strengthParts = Strength.Split('-');
+                    var min = Convert.ToInt32(strengthParts[0]);
+                    var max = Convert.ToInt32(strengthParts[1]) + 1;
+                    newStrength = random.Next(min, max);
+                    player2.Health -= newStrength;
+                    Console.WriteLine(Name + " tok " + newStrength + " skade fra " + player2.Name + ".");
+                }
+                else
+                {
+                    newStrength = Convert.ToInt32(Strength);
+                    player2.Health -= newStrength;
+                    Console.WriteLine(Name + " tok " + newStrength + " skade fra " + player2.Name + ".");
+                }
+                Stamina -= 10;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(player2.Name + " har " + player2.Health + "HP igjen.");
+            }
+
+            await Task.Delay(1000);
+        }
+
+        public void Recharge()
+        {
+            Stamina = MaxStamina;
+            Console.WriteLine(Name + " fikk " + MaxStamina);
+        }
+    }
+}
---
+++ Moodle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Moodle
+{
+    class Program
+    {
+
+        static GameCharacter hero = new GameCharacter("Arthas", 100, "20", 40);
+        static GameCharacter boss = new GameCharacter("Urug", 400, "0-30", 10);
+
+        static async Task Main(string[] args)
+        {
+            while (hero.Health > 0 && boss.Health > 0)
+            {
+                await hero.Fight(boss);
+                await boss.Fight(hero);
+            }
+
+            if (hero.Health == 0)
+            {
+                Console.WriteLine(boss.Name + " vant.");
+            }
+            
+            if (boss.Health == 0)
+            {
+                Console.WriteLine(hero.Name + " vant.");
+            }
+        }
+
+    }
+}
